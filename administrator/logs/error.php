3c2f3d520c5690d91b43085d68d12b4b8cff44e5
Nancy <nancy.nguyen@beyondedge.co>
2019-12-25 13:46:34 +0700

    test

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-12-25 06:23:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-12-25T06:23:10+00:00	INFO ::1	joomlafailure	Empty password not allowed.
